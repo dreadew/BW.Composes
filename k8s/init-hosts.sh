826c7513ac78b43b60d6e6c5b177a5d63dc47182
Артур Перов М <a.perov@bars-open.ru>
2025-05-17 23:01:01 +0300

    fixed k8s services and added bash scripts

---
+++ k8s/init-hosts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IP=$(minikube ip)
+HOSTNAMES=(
+    messaging.local
+    data.local
+    monitoring.local
+    secrets.local
+    storage.local
+)
+
+for HOSTNAME in "${HOSTNAMES[@]}"; do
+    if grep -q "$HOSTNAME" /etc/hosts; then
+        echo "host entry for $HOSTNAME already exists."
+    else
+        echo "adding host entry for $HOSTNAME..."
+        echo "$IP $HOSTNAME" | sudo tee -a /etc/hosts > /dev/null
+    fi
+done
+
+echo "hosts:"
+for HOSTNAME in "${HOSTNAMES[@]}"; do
+  echo "→ http://$HOSTNAME"
+done
---
+++ k8s/init-k8s-minikube.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Docker installed successfully."
+
+sudo usermod -aG docker $USER
+
+curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+kubectl version --client
+echo "Kubectl installed successfully."
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
+minikube version
+echo "Minikube installed successfully."
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+minikube start --driver=docker
+echo "Minikube started successfully."
+
+minikube addons enable ingress
+echo "Ingress plugin installed successfully."
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=Ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=180s
